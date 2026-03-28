20672a5b29e300316354364ad77956652224f79d
robertopalacio165-web <robertopalacio165@gmail.com>
2026-03-28 03:31:35 +0100

    Update Panel.tsx

--- artifacts/gestoria-cital-a/src/pages/Panel.tsx
+++ artifacts/gestoria-cital-a/src/pages/Panel.tsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import { useEffect, useState } from "react";
 import { Navbar } from "@/components/Navbar";
 import { LegalDisclaimer } from "@/components/LegalDisclaimer";
