117240bfff8f13e249348d85bd53a927d71ebc52
robertopalacio165-web <robertopalacio165@gmail.com>
2026-05-14 11:42:04 +0200

    Update Regularizacion2026.tsx

--- artifacts/gestoria-cital-a/src/pages/Regularizacion2026.tsx
+++ artifacts/gestoria-cital-a/src/pages/Regularizacion2026.tsx
@@ -2407,7 +2407,7 @@ disabled={!documentsUnlocked}
       )}
             </div>
           </div>
-   
+   )}
           <div className="flex flex-col gap-4">
             {paymentDoneRef.current && (
      <div className="rounded-2xl border border-white/10 bg-white/5 p-6 shadow-xl">
