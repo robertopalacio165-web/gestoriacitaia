27794ffe22bcab4a1f5c38601d542a2c8ad9cf2d
robertopalacio165-web <robertopalacio165@gmail.com>
2026-06-02 05:39:05 +0200

    Update search-worker.js

--- artifacts/gestoria-cital-a/worker/search-worker.js
+++ artifacts/gestoria-cital-a/worker/search-worker.js
@@ -44,10 +44,14 @@ async function runWorker() {
       =========================
       */
 
- browser = await chromium.launch({
-  headless: true
+browser = await chromium.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage"
+  ]
 });
-
 const page = await browser.newPage();
 
       await page.setViewportSize({
