f4801ff183a890a645df5bf5cf0a8b7de4f503af
robertopalacio165-web <robertopalacio165@gmail.com>
2026-07-20 15:25:15 +0200

    Update index.js

--- artifacts/gestoria-cital-a/worker-malta/index.js
+++ artifacts/gestoria-cital-a/worker-malta/index.js
@@ -1,5 +1,6 @@
 const log = require("./logger");
 const queue = require("./queue");
+const searchJobs = require("./search-jobs");
 
 async function start() {
 
@@ -7,6 +8,10 @@ async function start() {
     log("GESTORIACITAIA MALTA WORKER");
     log("=================================");
 
+    // Buscar ofertas nuevas
+    await searchJobs();
+
+    // Obtener clientes activos
     const applications = await queue.getPendingApplications();
 
     log(`Clientes activos: ${applications.length}`);
